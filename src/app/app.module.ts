30dade732e0eae10b42c1c8bf7bbcaa8b35bf977
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-06-08 18:30:34 +0100

    install store dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './share/header/header.component';
 import { NameComponent } from './components/list-names/name/name.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { StoreModule } from '@ngrx/store';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
