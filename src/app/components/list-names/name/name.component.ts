499137f5fd06aa35d0010fca18853fe62a7dd738
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-06-08 17:30:11 +0100

    add name component

---
+++ src/app/components/list-names/name/name.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name',
+  templateUrl: './name.component.html',
+  styleUrls: ['./name.component.css']
+})
+export class NameComponent implements OnInit {
+
+  @Input()
+  name?: string;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
