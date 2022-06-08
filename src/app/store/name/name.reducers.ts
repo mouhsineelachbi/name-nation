19efe8ebf7aa1110175b92a4af2ee1ef4cdb2bde
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-06-08 20:59:02 +0100

    change names to string in NamesState

--- src/app/store/name/name.reducers.ts
+++ src/app/store/name/name.reducers.ts
@@ -2,12 +2,12 @@ import { createReducer, on } from "@ngrx/store";
 import { FetchNames, FetchNamesFailed, FetchNamesSuccess } from "./name.actions";
 
 export interface NamesState {
-    names: string[]
+    names: string,
     errorMessage: string
 }
 
 export const initialState: NamesState = {
-    names: [],
+    names: '',
     errorMessage: '',
 };
 
