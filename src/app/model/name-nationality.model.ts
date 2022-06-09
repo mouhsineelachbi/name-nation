5599778605e2631160d73c2afd4881592bb9154b
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-06-09 19:22:03 +0100

    create nameNationality class implementing name nationality interface

--- src/app/model/name-nationality.model.ts
+++ src/app/model/name-nationality.model.ts
@@ -4,3 +4,14 @@ export interface NameNationality {
     name: string;
     country: Country[];
 }
+
+export class NameNationalityC implements NameNationality {
+    name: string;
+    country: Country[];
+
+    constructor(name: string, country: Country[]){
+        this.name = name;
+        this.country = country;
+
+    }
+}
