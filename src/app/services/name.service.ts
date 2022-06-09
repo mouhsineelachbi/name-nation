340a17e24ae5fbeb5bca55bd7ce165e2981203dc
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-06-09 13:16:56 +0100

    create service function to get names nationality

--- src/app/services/name.service.ts
+++ src/app/services/name.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { map, Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { NameNationality } from '../model/name-nationality.model';
 import { AppState } from '../store/app.states';
@@ -16,24 +16,29 @@ export class NameService {
   constructor(private http: HttpClient, private store: Store<AppState>) {
     this.apiUrl = environment['baseUrl'];
     this.nationalizeUrl = environment['nationalizeUrl'];
-
   }
 
-  getNames(): Observable<string>{
+  getNames(): Observable<string> {
     const headers = new HttpHeaders().set(
       'Content-Type',
       'text/plain; charset=utf-8'
     );
-    return this.http
-      .get(this.apiUrl + 'getNames', {
-        headers,
-        responseType: 'text',
-      })
+    return this.http.get(this.apiUrl + 'getNames', {
+      headers,
+      responseType: 'text',
+    });
   }
 
   getNamesNationality(): Observable<NameNationality> {
-    let name:string[] = []
-    this.store.select((state)=> state.names.names).subscribe(res => name = res)
-    return this.http.get<NameNationality>(this.nationalizeUrl)
+    let name: string[] = [];
+    let allNames: string = ''
+    this.store
+      .select((state) => state.names.names)
+      .subscribe((names) => {
+        name = names.map((name) => 'name[]=' + name + '&');
+        allNames = name.join("");
+        console.log(allNames)
+      });
+    return this.http.get<NameNationality>(this.nationalizeUrl+allNames);
   }
 }
