4190976111c2905a6226107a00261f13950da799
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-06-09 13:23:19 +0100

    create name nationality selector

---
+++ src/app/store/name-nationality/name-nationality.selectors.ts
@@ -0,0 +1,15 @@
+import { createSelector } from "@ngrx/store";
+import { AppState } from "../app.states";
+import { NameNationalityState } from "./name-nationality.reducers";
+
+export const selectNameNationalityState = (state: AppState) => state.namesNationality
+
+export const selectNamesNationality = createSelector(
+    selectNameNationalityState,
+    (state: NameNationalityState) => state.nameNationality
+)
+
+export const selectError = createSelector(
+    selectNameNationalityState,
+    (state: NameNationalityState) => state.errorMessage
+)
