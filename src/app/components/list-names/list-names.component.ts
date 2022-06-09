6ab203f65d73341910989765094a61b037b759a9
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-06-09 19:20:38 +0100

    subscribe to sucess name nationalize action to  get names nationalities

--- src/app/components/list-names/list-names.component.ts
+++ src/app/components/list-names/list-names.component.ts
@@ -2,9 +2,13 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { ToastrService } from 'ngx-toastr';
-import { map } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+import { NameNationality } from 'src/app/model/name-nationality.model';
 import { NameService } from 'src/app/services/name.service';
 import { AppState } from 'src/app/store/app.states';
+import {
+  FetchNameNationality, NameNationalityActionTypes
+} from 'src/app/store/name-nationality/name-nationality.actions';
 import { FetchNames, NameActionTypes } from 'src/app/store/name/name.actions';
 
 @Component({
@@ -13,13 +17,9 @@ import { FetchNames, NameActionTypes } from 'src/app/store/name/name.actions';
   styleUrls: ['./list-names.component.css'],
 })
 export class ListNamesComponent implements OnInit {
-  names?: string[];
-  error?: string;
-  getError = this.store
-    .select((state) => state.names.errorMessage)
-    .pipe((error) => error);
-  getNames = this.store
-    .select((state) => state.names.names)
+
+  namesNationalities?: NameNationality[]
+  isLoading = this.store.select((state)=> state?.namesNationality?.loading)
 
   constructor(
     private service: NameService,
@@ -29,18 +29,27 @@ export class ListNamesComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    /*
+     * dispatch fetch names to get names from JAVA Rest API
+     */
     this.store.dispatch(FetchNames());
+
+    /*
+     * Listen to success action to dispatch fetch nationality of names
+     */
     this.actions
       .pipe(ofType(NameActionTypes.FETCH_NAMES_SUCCESS))
       .subscribe(() =>
         this.store
           .select((state) => state.names.names)
-          .subscribe((names) => {
-            this.names = names;
-            this.showSuccess();
+          .subscribe(() => {
+            this.store.dispatch(FetchNameNationality());
           })
       );
 
+    /*
+     * Listen to failed action to get error
+     */
     this.actions
       .pipe(ofType(NameActionTypes.FETCH_NAMES_FAILED))
       .subscribe(() =>
@@ -50,7 +59,21 @@ export class ListNamesComponent implements OnInit {
             this.showFailed(error);
           })
       );
+
+    /*
+     * Listen to success action to get names nationalities
+     */
+    this.actions
+      .pipe(ofType(NameNationalityActionTypes.FETCH_NAMENATIONALITY_SUCCESS))
+      .subscribe((names:any) => {
+        console.log(names.nameNationality);
+        this.namesNationalities = names.nameNationality
+      })
   }
+
+  /*
+   * use Toast package to show success notification
+   */
   showSuccess() {
     this.toastr.success(
       'Names has been successfully fetched from api',
@@ -61,6 +84,9 @@ export class ListNamesComponent implements OnInit {
     );
   }
 
+  /*
+   * use Toast package to show error notification
+   */
   showFailed(error: string) {
     this.toastr.error(error, 'Error', {
       progressBar: true,
