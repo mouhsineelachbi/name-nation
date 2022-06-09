d086d690ce89a25de3b3f1f683557175ee93a685
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-06-09 19:21:23 +0100

    create country class implementing country interface

--- src/app/model/country.model.ts
+++ src/app/model/country.model.ts
@@ -2,3 +2,12 @@ export interface Country {
     country_id: string;
     probability: number;
 }
+
+export class CountryC implements Country {
+    country_id: string;
+    probability: number;
+    constructor(country_id: string, probability: number){
+        this.country_id = country_id
+        this.probability = probability
+    }
+}
