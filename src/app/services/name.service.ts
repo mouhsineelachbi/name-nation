e042b4d20f88781658b009ded5504491a1bc0549
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-06-10 01:12:36 +0100

    send multiple request to get nationality of each name

--- src/app/services/name.service.ts
+++ src/app/services/name.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { map, Observable, of, tap } from 'rxjs';
+import { map, Observable, of, tap, switchMap, combineLatest } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ArrayManipulation } from '../helpers/array.manipulation';
 import { NameNationality } from '../models/name-nationality.model';
 import { AppState } from '../store/app.states';
 
@@ -13,11 +14,18 @@ export class NameService {
   apiUrl: string;
   nationalizeUrl: string;
 
-  constructor(private http: HttpClient, private store: Store<AppState>) {
+  constructor(
+    private http: HttpClient,
+    private store: Store<AppState>,
+    private arrayManipulation: ArrayManipulation
+  ) {
     this.apiUrl = environment['baseUrl'];
     this.nationalizeUrl = environment['nationalizeUrl'];
   }
 
+  /*
+  *   Get names from Java rest API as text format
+  */
   getNames(): Observable<string> {
     const headers = new HttpHeaders().set(
       'Content-Type',
@@ -29,16 +37,31 @@ export class NameService {
     });
   }
 
-  getNamesNationality(): Observable<NameNationality> {
-    let name: string[] = [];
-    let allNames: string = ''
+  /*
+  *   get names from store and join then to be in form name[]=Name&
+  *   slice the array into slices with chunch size of 10
+  *   then combine multiple observables to call api each time
+  */
+  getNamesNationality() {
+    let ArrayOfNamesArrays: string[][] = [];
+    let allNames: string[] = [];
     this.store
       .select((state) => state.names.names)
       .subscribe((names) => {
-        name = names.map((name) => 'name[]=' + name + '&');
-        allNames = name.join("");
-        console.log(allNames)
+        ArrayOfNamesArrays = this.arrayManipulation.getChunkFromArray(
+          names,
+          10
+        );
+        allNames = ArrayOfNamesArrays.map((namesArray: string[]) => {
+          let onOperationArray = namesArray.map(
+            (name) => 'name[]=' + name + '&'
+          );
+          return onOperationArray.join('');
+        });
       });
-    return this.http.get<NameNationality>(this.nationalizeUrl+allNames);
+
+    return combineLatest(
+      allNames.map((names) => this.http.get(this.nationalizeUrl + names))
+    )
   }
 }
